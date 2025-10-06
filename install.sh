136c4852708e4851f437441a12eb68564e573369
Chronois485 <kmm15072011@gmail.com>
2025-10-06 19:02:50 +0300

    added my hyprland config

--- install.sh
+++ install.sh
@@ -5,17 +5,19 @@ cp ~/.zshrc ./.zshrc.bak -r
 cp ~/.config/omarchy ./omarchy.bak -r
 cp ~/.config/walker ./walker.bak -r
 cp ~/.config/waybar ./waybar.bak -r
+cp ~/.config/hypr ./hypr.bak -r
 
 echo "Deleting old config..."
 
-rm ~/.config/nvim ~/.config/omarchy ~/.config/walker/config.toml ~/.config/waybar/config.jsonc -r
+rm ~/.config/nvim ~/.config/omarchy ~/.config/walker/config.toml ~/.config/waybar/config.jsonc ~/.zshrc ~/.config/hypr -r
 
 echo "Applying new config..."
 
 cp ./nvim ~/.config/ -r
-cp ./.zshrc ~/.zshrc -r 
+cp ./.zshrc ~/.zshrc -r
 cp ./omarchy ~/.config/ -r
-cp ./walker ~/.config/ -r 
+cp ./walker ~/.config/ -r
 cp ./waybar ~/.config/ -r
+cp ./hypr ~/.config/ -r
 
 echo "Everything is done!"
