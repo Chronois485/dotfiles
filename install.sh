27f9ca49f61bdb95c45a1f0ac27ad23a5f0bcbb1
Chronois485 <kmm15072011@gmail.com>
2025-10-31 14:07:27 +0200

    completed install script

--- install.sh
+++ install.sh
@@ -12,3 +12,20 @@ rm "~/.config/omarchy" -r
 rm "~/.config/walker" -r
 rm "~/.config/waybar" -r
 rm "~/.zshrc"
+echo "Applying new config"
+cp "./hypr" "~/.config/hypr" -r
+cp "./nvim" "~/.config/nvim" -r
+cp "./omarchy" "~/.config/omarchy" -r
+cp "./walker" "~/.config/walker" -r
+cp "./waybar" "~/.config/waybar" -r
+cp "./zshrc" "~/.zshrc"
+echo "Launching full system update"
+sudo pacman -Syu --noconfirm
+echo "Installing needed packages"
+sudo pacman -S --noconfirm zsh cava cmatrix fzf thefuck eza zoxide walker waybar neovim elephant python npm nodejs lua figlet btop
+echo "Installing AUR packages"
+yay -S --noconfirm tenki
+echo "Setting zsh as main terminal"
+sudo chsh --shell /usr/bin/zsh $USER
+echo "All done!"
+echo "Reboot pc to confirm changes" 
