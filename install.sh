0df42a31a3cd9538a0278d639bee5a51c71a19e4
Chronois485 <kmm15072011@gmail.com>
2025-11-23 21:05:00 +0200

    added fonts and network manager

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 echo "Creating backups"
-cp "~/.config/hypr" "~/.config/hypr.bak" -r
-cp "~/.config/nvim" "~/.config/nvim.bak" -r
-cp "~/.config/omarchy" "~/.config/omarchy.bak" -r
-cp "~/.config/walker" "~/.config/walker.bak" -r
-cp "~/.config/waybar" "~/.config/waybar.bak" -r
-cp "~/.zshrc" "~/.zshrc.bak"
+cp ~/.config/hypr ~/.config/hypr.bak -r
+cp ~/.config/nvim ~/.config/nvim.bak -r
+cp ~/.config/omarchy ~/.config/omarchy.bak -r
+cp ~/.config/walker ~/.config/walker.bak -r
+cp ~/.config/waybar ~/.config/waybar.bak -r
+cp ~/.zshrc ~/.zshrc.bak
 echo "Deleting old config"
 rm ~/.config/hypr -r
 rm ~/.config/nvim -r
@@ -19,13 +19,18 @@ cp ./omarchy ~/.config/omarchy -r
 cp ./walker ~/.config/walker -r
 cp ./waybar ~/.config/waybar -r
 cp ./zshrc ~/.zshrc
+cp ./fonts/* ~/.local/share/fonts/
 echo "Launching full system update"
 sudo pacman -Syu --noconfirm
 echo "Installing needed packages"
-sudo pacman -S --noconfirm zsh cava cmatrix fzf thefuck eza zoxide walker waybar neovim elephant python npm nodejs lua figlet btop
+sudo pacman -S --noconfirm zsh cava cmatrix fzf thefuck eza zoxide walker waybar neovim elephant python npm nodejs lua figlet btop networkmanager
 echo "Installing AUR packages"
-yay -S --noconfirm tenki
+yay -S --noconfirm tenki gazelle-tui
 echo "Setting zsh as main terminal"
 sudo chsh --shell /usr/bin/zsh $USER
+echo "Switching from iwd to network manager"
+sudo systemctl disable iwd.service
+sudo systemctl enable NetworkManager-initrd.service
+sudo systemctl enable NetworkManager.service
 echo "All done!"
 echo "Reboot pc to confirm changes" 
--- sync.sh
+++ sync.sh
@@ -1,23 +1,18 @@
 echo "Creating backups"
-cp "./hypr" "./hypr.bak" -r
-cp "./nvim" "./nvim.bak" -r
-cp "./omarchy" "./omarchy.bak" -r
-cp "./walker" "./walker.bak" -r
-cp "./waybar" "./waybar.bak" -r
-cp "./zshrc" "./zshrc.bak"
-echo "Deleting old config"
-rm "./hypr" -r
-rm "./nvim" -r
-rm "./omarchy" -r
-rm "./walker" -r
-rm "./waybar" -r
-rm "./zshrc"
+mv hypr ./hypr.bak
+mv ./nvim ./nvim.bak
+mv ./omarchy ./omarchy.bak
+mv ./walker ./walker.bak
+mv ./waybar ./waybar.bak
+mv ./fonts ./fonts.bak
+mv ./zshrc ./zshrc.bak
 echo "Copying new config"
 cp ~/.config/hypr ./ -r
 cp ~/.config/nvim ./ -r
 cp ~/.config/omarchy ./ -r
 cp ~/.config/walker ./ -r
 cp ~/.config/waybar ./ -r
+cp ~/.local/share/fonts ./ -r
 cp ~/.zshrc ./zshrc
 echo "Deleting other git repos"
 find ./omarchy -type d -name ".git" -exec rm -rf {} +
